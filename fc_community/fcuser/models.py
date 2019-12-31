ef4b39cb112365620bb62f70665e25f82700b77a
Seono <pianist5555@gmail.com>
2020-01-01 07:15:59 +0900

    0101 happyNewYear
    commit

---
+++ fc_community/fcuser/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+class Fcuser(models.Model):
+    username = models.CharField(max_length=32,
+                                verbose_name='사용자명')
+    password = models.CharField(max_length=64,
+                                verbose_name='비밀번호')
+    registerd_dttm = models.DateTimeField(auto_now_add=True,
+                                          verbose_name='등록시간')
+    
+    class Meta:
+        db_table = 'fastcampus_fcuser'
