6315d403d4d1b771fa34fcdcef28e9b498bbb90c
Seono <pianist5555@gmail.com>
2020-01-01 11:37:00 +0900

    0101 register
    Exception

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.hashers import make_password
 from .models import Fcuser
 
 # Create your views here.
@@ -7,19 +8,24 @@ from .models import Fcuser
 def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
+
     elif request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        re_password = request.POST['re-password']
+        username = request.POST.get('username', None) 
+        password = request.POST.get('password', None)
+        re_password = request.POST.get('re-password', None)
 
+        res_data = {}
+
+        if not (username and password and re_password):
+             res_data['error'] = '모든 값을 입력해야합니다.'
         if password != re_password:
-            return HttpResponse('실패')
+            res_data['error'] = '비밀번호가 다릅니다.'
+        else:
+            fcuser = Fcuser(
+                username = username,
+                password = make_password(password)
+            )
 
-        fcuser = Fcuser(
-            username = username,
-            password = password
-        )
+            fcuser.save()
 
-        fcuser.save()
-
-        return render(request, 'register.html')
+        return render(request, 'register.html', res_data)
