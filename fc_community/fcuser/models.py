92cdc9426c6a887c2217de90c82427f5f200c7b0
Seono <pianist5555@gmail.com>
2020-01-01 12:00:36 +0900

    0101 register
    adding email column

--- fc_community/fcuser/models.py
+++ fc_community/fcuser/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Fcuser(models.Model):
     username = models.CharField(max_length=32,
                                 verbose_name='사용자명')
+    usermail = models.EmailField(max_length=128,
+                                verbose_name='사용자이메일')
     password = models.CharField(max_length=64,
                                 verbose_name='비밀번호')
     registerd_dttm = models.DateTimeField(auto_now_add=True,
--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -12,18 +12,20 @@ def register(request):
     elif request.method == 'POST':
         username = request.POST.get('username', None) 
         password = request.POST.get('password', None)
+        usermail = request.POST.get('usermail', None)
         re_password = request.POST.get('re-password', None)
 
         res_data = {}
 
-        if not (username and password and re_password):
+        if not (username and usermail and password and re_password):
              res_data['error'] = '모든 값을 입력해야합니다.'
         if password != re_password:
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
             fcuser = Fcuser(
                 username = username,
-                password = make_password(password)
+                password = make_password(password),
+                usermail = usermail
             )
 
             fcuser.save()
