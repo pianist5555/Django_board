8d8b77ceed497f7f3149a7cd9daac158e45615d6
Seono <pianist5555@gmail.com>
2020-01-01 11:19:25 +0900

    0101 Django 분기처리

--- fc_community/fcuser/views.py
+++ fc_community/fcuser/views.py
@@ -1,6 +1,25 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Fcuser
 
 # Create your views here.
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        re_password = request.POST['re-password']
+
+        if password != re_password:
+            return HttpResponse('실패')
+
+        fcuser = Fcuser(
+            username = username,
+            password = password
+        )
+
+        fcuser.save()
+
+        return render(request, 'register.html')
