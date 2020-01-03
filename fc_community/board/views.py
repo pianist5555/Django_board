910e1ede14ae60f4487807c69aeaf3c6648357da
Seono <pianist5555@gmail.com>
2020-01-03 10:53:51 +0900

    0103 board
    setting models && params

--- fc_community/board/views.py
+++ fc_community/board/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from.models import Board
 
 def board_list(request):
-    return render(request, 'board_list.html')
+    boards = Board.objects.all().order_by('-id') # id 시간역순으로 정렬 = 최신순
+    return render(request, 'board_list.html', {'boards' : boards})
